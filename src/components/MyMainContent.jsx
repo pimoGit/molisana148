eda9bafcd67fea5c30ed4e013c0690b0b655ccab
Simone <info@simoneicardi.com>
2025-09-25 12:09:11 +0200

    scorpora componente di listato prodotti

--- src/components/MyMainContent.jsx
+++ src/components/MyMainContent.jsx
@@ -1,10 +1,9 @@
+import ProductList from "./product/ProductList"
+
 const MyMainContent = () => {
     return (
         <main>
-            <section id="products">
-                <h2>Lista prodotti</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, sapiente itaque. Iste ut consequatur et vel, dolor quae dignissimos similique delectus nobis nam. Praesentium nam quibusdam cumque recusandae, optio ullam.</p>
-            </section>
+            <ProductList />
         </main>
     )
 }
