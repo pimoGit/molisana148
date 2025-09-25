eebd546fd8961190282b6524568f645f3b605348
Simone <info@simoneicardi.com>
2025-09-25 11:12:42 +0200

    dinamicizza voci navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,22 @@
 const Navbar = () => {
+
+    const links = [
+        { id: 1, text: 'Home', url: '#', current: false },
+        { id: 2, text: 'Prodotti', url: '#', current: true },
+        { id: 3, text: 'Chi Siamo', url: '#', current: false },
+        { id: 4, text: 'Contatti', url: '#', current: false },
+    ];
+
     return (
         <nav>
             <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#" className="active">Prodotti</a></li>
-                <li><a href="#">Chi Siamo</a></li>
-                <li><a href="#">Contatti</a></li>
+                {links.map(link => (
+                    <li key={link.id}>
+                        <a href={link.url} className={link.current ? "active" : ""}>
+                            {link.text}
+                        </a>
+                    </li>
+                ))}
             </ul>
         </nav>
     )
