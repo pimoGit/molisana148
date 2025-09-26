327542ccc8e92d2e9125eb2ca5a5e281d97129db
Simone <info@simoneicardi.com>
2025-09-26 12:04:34 +0200

    dumb component con children props

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import MyHeader from "./components/MyHeader"
 import MyMainContent from "./components/MyMainContent"
 import MyFooter from "./components/MyFooter"
+import Alert from "./components/Alert"
 import './App.css'
 
 function App() {
@@ -8,6 +9,13 @@ function App() {
     return (
         <>
             <MyHeader />
+            <Alert type="error" >
+                <ul>
+                    <li>Error 1</li>
+                    <li>Error 2</li>
+                    <li>Error 3</li>
+                </ul>
+            </Alert>
             <MyMainContent />
             <MyFooter />
         </>
